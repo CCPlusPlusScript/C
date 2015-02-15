b192531350b67836d3c32b6cd026fdf6a85c2dfd
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-02-15 15:34:39 -0500

    Size of Int, long,float, short

--- CPractice/main.c
+++ CPractice/main.c
@@ -9,7 +9,11 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    // Size of Int, long,float, short
+        printf("Size of int : %d \n", sizeof(int));
+        printf("Size of float : %d \n", sizeof(float));
+        printf("Size of char : %d \n", sizeof(char));
+        printf("Size of short : %d \n", sizeof(short));
+        printf("Size of long : %d \n", sizeof(long));
     return 0;
 }
