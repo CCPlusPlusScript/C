099ade478faf0e192ca172b390392f1104c1aaa8
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-02-15 14:22:49 -0500

    First Commit (Base Structure)

---
+++ CPractice/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CPractice
+//
+//  Created by Jagdeep Matharu on 2015-02-15.
+//  Copyright (c) 2015 Jagdeep Matharu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
