93d9359dc9e47ebb0a11f344944ead1b3ace34c9
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-02-16 12:57:27 -0500

    Special Characters

--- CPractice/main.c
+++ CPractice/main.c
@@ -8,12 +8,21 @@
 
 #include <stdio.h>
 
+
+#define NEWLINE = '\n';
+
 int main(int argc, const char * argv[]) {
+    #define i = 10;
+    
     // Size of Int, long,float, short
         printf("Size of int : %d \n", sizeof(int));
         printf("Size of float : %d \n", sizeof(float));
+        printf("\v");
         printf("Size of char : %d \n", sizeof(char));
+        printf("\f");
         printf("Size of short : %d \n", sizeof(short));
+        printf("%c", NEWLINE);
         printf("Size of long : %d \n", sizeof(long));
+    
     return 0;
 }
